1db549690899cb47d3fa888d988eaa2eaf0e1a6c
Pablo Sanchez <psnsanchez@gmail.com>
2021-04-28 14:34:24 -0300

    Subiendo programa dado

---
+++ Dado.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace juegoDados
+{
+    class Dado
+    {
+        private int lados { get; set; }
+        public Dado()
+        {
+
+        }
+        public Dado(int lados)//constructor
+        {
+            this.lados = lados;
+        }
+
+        public void setLados(int lados)
+        {
+            this.lados = lados;
+        }
+        public int getLados()
+        {
+            return this.lados;
+        }
+        //Metodo que selecciona un numero entre el rango de [1, 6] y compara con el elegido
+        protected internal String numeroDado(int nroElegido)
+        {
+            Random r = new Random();
+            int numero = r.Next(1, this.lados);
+
+            if (nroElegido == numero)
+            {
+                return "CORRECTO, el numero es " + numero;
+            }
+            else
+            {
+                return "Casi casi pa, el numero que salio es " + numero.ToString();
+            }
+
+        }
+        //Metodo de comprobacion numero ingresado
+        protected internal int comprobacionNumero(int numeroElegido)
+        {
+            while ((numeroElegido < 1) || (numeroElegido > this.lados))
+            {
+                Console.WriteLine("Error, seleccione un numero valido del 1 al {0}",this.lados);
+                numeroElegido = int.Parse(Console.ReadLine());
+            }
+            return numeroElegido;
+        }
+
+        public int ingresoLados()
+        {
+            Boolean condicion = false;
+
+            while (condicion == false)
+            {
+                lados = int.Parse(Console.ReadLine());
+                if ((lados >= 4) && (lados <= 20))
+                {
+                    condicion = true;
+                    Console.WriteLine("Ud ha seleccionado un dado de {0} lados\n", lados);                    
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("El numero minimo es de 4 y el maximo de 20\n");
+                    Console.WriteLine("Ingrese nuevamente: ");
+                }
+            }
+            return lados;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace juegoDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean condicion = false;
+            Dado eldado = new Dado();            
+
+            while (condicion == false)
+            {
+                Console.WriteLine("Cuantos lados tiene su dado?\n");
+                try
+                {
+                    eldado.ingresoLados();
+                    break;
+                }
+                catch (System.FormatException letras)
+                {
+                    Console.WriteLine("Error de Tipo de dato: " + letras.Message + "\n");
+                }
+                catch (System.OverflowException grande)
+                {
+                    Console.WriteLine("Error de Sobredimension: " + grande.Message + "\n");
+                }
+                Console.WriteLine("\n - - - - - - - - - - - - - - - - - - - \n");
+            }
+                    
+            
+            //Pedimos el ingreso de un numero para la apuesta
+            Console.WriteLine("Hola, elija un numero del 1 al {0}: ", eldado.getLados());
+            try
+            {
+                int nroElegido = int.Parse(Console.ReadLine());
+                //Comprobamos si el numero ingresado es valido
+                eldado.comprobacionNumero(nroElegido);
+
+                //Si el numero es valido comparamos y mostramos el resultado
+                Console.WriteLine(eldado.numeroDado(nroElegido));
+            }            
+            catch (System.FormatException letras)
+            {
+                Console.WriteLine("Error de Tipo de dato: "+letras.Message);
+            }
+            catch (System.OverflowException grande)
+            {
+                Console.WriteLine("Error de Sobredimension: "+grande.Message);
+            }                        
+            Console.ReadKey();
+        }
+    }   
+}
